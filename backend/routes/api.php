f0261916daa1552ea67413e86e7635f9f1974e64
Muhammad Iqbal <hellofromiqbal@gmail.com>
2026-02-22 14:43:21 +0700

    feat: add simple login system for admin (hardcoded credential to simplify the test project)

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestaurantController;
+use App\Http\Controllers\AuthController;
 
+Route::post('/login', [AuthController::class, 'login']);
 Route::get('/restaurants', [RestaurantController::class, 'index']);
 Route::get('/restaurants/{id}', [RestaurantController::class, 'show']);
-Route::post('/restaurants', [RestaurantController::class, 'store']);
-Route::delete('/restaurants/{id}', [RestaurantController::class, 'destroy']);
+Route::middleware('auth:sanctum')->group(function() {
+  Route::post('/restaurants', [RestaurantController::class, 'store']);
+  Route::delete('/restaurants/{id}', [RestaurantController::class, 'destroy']);
+});
